746613d26f2fb8f6f1b39c8e8f891e371cb49b6a
vkmindia80@gmail.com <vkmindia80@gmail.com>
2026-04-23 06:13:09 -0400

    t4

--- Python DataTypes.py
+++ Python DataTypes.py
@@ -9,6 +9,28 @@ Sequence Types: list, tuple, range
 Mapping Type: dictSet Types: set, frozenset
 Boolean Type: Bool True,False
 Binary Types: bytes, bytearray, memoryview
+
+Setting the Specific Data Type
+If you want to specify the data type, you can use the following constructor
+functions:
+x = "Hello World" //str
+x = 20 //int
+x = 20.5 //float
+x = 1j //complex
+x = ["apple", "banana", "cherry"] //list
+x = ("apple", "banana", "cherry") //tuple
+x = range(6) //range
+x = {"name" : "John", "age" : 36} //dict
+x = {"apple", "banana", "cherry"} //set
+x = frozenset({"apple", "banana", "cherry"}) //frozenset
+x = True //bool
+x = b"Hello" //bytes
+x = bytearray(5) //bytearray
+x = memoryview(bytes(5)) //memoryview
+
+Getting the Data Type
+You can get the data type of any variable by using the type() function:
+Example
 '''
 x = 5
 print(type(x))
@@ -167,4 +189,42 @@ sum = a + b
 print ( "first no = ", a )
 print ( "second no=", b )
 print ( "sum = ", sum )
+
+Try:
+Ex1:
+Accept student details
+rollno, name, marks1, marks2, marks3. And calculate total and average.
+Ex2: Complete the given program
+x = 5
+y = 10
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+Ex3: Try the given examples.
+1. Calculate area of circle formula : 2 .
+2. Calculate area of square (side * side).
+3. Accept year of birth from the keyboard and print How old he is.
+4. Calculate cube of given number for input.
+5. Accept person details firstName, lastName, email, phone print fullName ,
+email, Phone with Labels.
+6. Take Productname, prince, quantity from input print cost. Of the product.
+7. Convert kilometres to miles: 1 mile =1.6 km. 7 km = ? miles.
+Number of miles = Km/1.6.
+8. Accept student marks m1,m2,m3 from keyboard and Calculate total and
+avg. Print avg using float data type.
+9. Take a rupees from keyboard and print dollars.
+What you learn
+ Built-in Data Types
+ Getting the Data Type Setting the Specific Data Type
+ Python Numbers
+ Int
+ Complex
+ Type Conversion
+ Random Number
+ Python Casting
+
 '''
