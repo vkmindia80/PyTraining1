280a825eaafe91eb177ec76c449cb54aa8aa0e5e
vkmindia80@gmail.com <vkmindia80@gmail.com>
2026-04-25 06:44:09 -0400

    t43

---
+++ Python Loops.py
@@ -0,0 +1,18 @@
+'''Python has two primitive loop commands:
+ while loops
+ for loops
+The while Loop
+With the while loop we can execute a set of statements as long as a
+condition is true.
+
+Syntax
+while condition:
+# Indented code block to execute
+statement(s)
+Example
+Print i as long as i is less than 6:
+'''
+i = 1
+while i < 6:
+    print(i)
+i += 1
