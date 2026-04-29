548d50c9dc45c5666e85321a9262aec3ef36fa69
vkmindia80@gmail.com <vkmindia80@gmail.com>
2026-04-29 06:54:11 -0400

    t234

--- Python Dictionaries.py
+++ Python Dictionaries.py
@@ -1,1 +1,582 @@
 #Python Dictionaries
+'''Dictionary
+Dictionaries are used to store data values in key:value pairs.
+A dictionary is a collection which is ordered*, changeable and do not allow
+duplicates.
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier,
+dictionaries are unordered.
+Dictionaries are written with curly brackets, and have keys and values:
+Example
+Create and print a dictionary:
+# Using curly braces {}
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+print(thisdict)
+
+Dictionary Items
+Dictionary items are ordered, changeable, and does not allow duplicates.
+Dictionary items are presented in key:value pairs, and can be referred to by
+using the key name.
+
+Example
+Print the "brand" value of the dictionary:
+
+thisdict = {
+"brand": "Ford","model": "Mustang",
+"year": 1964
+}
+print(thisdict["brand"])
+print(thisdict["model"])
+print(thisdict["year"])
+
+Ordered or Unordered?
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier,
+dictionaries are unordered.
+When we say that dictionaries are ordered, it means that the items have a
+defined order, and that order will not change.
+Unordered means that the items does not have a defined order, you cannot
+refer to an item by using an index.
+
+Changeable
+Dictionaries are changeable, meaning that we can change, add or remove
+items after the dictionary has been created.
+Duplicates Not Allowed
+Dictionaries cannot have two items with the same key:
+Example
+Duplicate values will overwrite existing values:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964,
+"year": 2020}
+print(thisdict)
+print(thisdict["brand"])
+print(thisdict["model"])
+print(thisdict["year"])
+
+Dictionary Length
+To determine how many items a dictionary has, use the len() function:
+Example
+Print the number of items in the dictionary:
+
+print(len(thisdict))
+
+Dictionary Items - Data Types
+The values in dictionary items can be of any data type:
+Example
+String, int, boolean, and list data types:
+
+thisdict = {
+"brand": "Ford",
+"electric": False,
+"year": 1964,
+"colors": ["red", "white", "blue"]
+}
+print(thisdict)
+print(thisdict["brand"])
+print(thisdict["electric"])
+print(thisdict["year"])
+print(thisdict["colors"])
+print(len(thisdict))
+
+type()From Python's perspective, dictionaries are defined as objects with the data
+type 'dict':
+<class 'dict'>
+Example
+Print the data type of a dictionary:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+print(type(thisdict))
+print(type(thisdict["brand"]))
+print(type(thisdict["model"]))
+print(type(thisdict["year"]))
+
+Access Dictionary Items
+Accessing Items
+You can access/read the items of a dictionary by referring to its key name,
+inside square brackets:
+Example
+Get the value of the "model" key:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = thisdict["brand"]
+print(x)
+There is also a method called get() that will give you the same result:
+Example
+Get the value of the "model" key:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = thisdict.get("model")
+print(x)
+
+Get Keys
+The keys() method will return a list of all the keys in the dictionary.
+Example
+Get a list of the keys:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = thisdict.keys()
+print(x)
+
+The list of the keys is a view of the dictionary, meaning that any changes
+done to the dictionary will be reflected in the keys list.
+Example
+Add a new item to the original dictionary, and see that the keys list gets
+updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x) #before the change
+car["color"] = "white"
+print(x) #after the change
+
+Get Values
+The values() method will return a list of all the values in the dictionary.
+Example
+Get a list of the values:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = thisdict.keys()
+print(x)
+y = thisdict.values()
+print(y)
+
+The list of the values is a view of the dictionary, meaning that any changes
+done to the dictionary will be reflected in the values list.
+Example
+Make a change in the original dictionary, and see that the values list gets
+updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
+
+Example
+Add a new item to the original dictionary, and see that the values list gets
+updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
+Get Items
+The items() method will return each item in a dictionary, as tuples in a list.
+Example
+Get a list of the key:value pairs
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x)
+
+The returned list is a view of the items of the dictionary, meaning that any
+changes done to the dictionary will be reflected in the items list.
+Example
+Make a change in the original dictionary, and see that the items list gets
+updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
+
+Example
+Add a new item to the original dictionary, and see that the items list gets
+updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
+Check if Key Exists
+To determine if a specified key is present in a dictionary use the in keyword:
+Example
+Check if "model" is present in the dictionary:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964}
+if "model" in thisdict:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+Change Dictionary Items
+Change Values
+You can change the value of a specific item by referring to its key name:
+Example
+Change the "year" to 2018:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+Update Dictionary
+The update() method will update the dictionary with the items from the given
+argument.
+The argument must be a dictionary, or an iterable object with key:value
+pairs.
+ExampleUpdate the "year" of the car by using the update() method:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.update({"year": 2020})
+print(thisdict)
+
+Add Dictionary Items
+Adding Items
+Adding an item to the dictionary is done by using a new index key and
+assigning a value to it:
+Example
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+Update DictionaryThe update() method will update the dictionary with the items from a given
+argument. If the item does not exist, the item will be added.
+The argument must be a dictionary, or an iterable object with key:value
+pairs.
+Example
+Add a color item to the dictionary by using the update() method:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.update({"color": "red"})
+print(thisdict)
+
+Remove Dictionary Items
+Removing Items
+There are several methods to remove items from a dictionary:
+Example
+The pop() method removes the item with the specified key name:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+Example
+The popitem() method removes the last inserted item (in versions before 3.7,
+a random item is removed instead):
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+thisdict.popitem()
+print(thisdict)
+
+Example
+The del keyword removes the item with the specified key name:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+Example
+The del keyword can also delete the dictionary completely:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang","year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+Example
+The clear() method empties the dictionary:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+Loop Dictionaries
+Loop Through a Dictionary
+You can loop through a dictionary by using a for loop.
+When looping through a dictionary, the return value are the keys of the
+dictionary, but there are methods to return the values as well.
+Example
+Print all key names in the dictionary, one by one:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+# Example: Print all keys in the dictionary, one by one:
+for x in thisdict:
+    print(x)
+# Example: Print all keys in the dictionary, one by one:
+for x in thisdict.keys():
+    print(x)
+# Example: Print all values in the dictionary, one by one:
+for x in thisdict:
+    print(thisdict[x])
+# Example: Print all values in the dictionary, one by one:
+for x in thisdict.values():
+    print(x)
+# Example: Print all keys and values in the dictionary, one by one:
+for x in thisdict:
+    print(x, thisdict[x])
+# Example: Loop through both keys and values, by using the items() method:
+for x, y in thisdict.items():
+    print(x, y)
+
+Copy Dictionaries
+Copy a Dictionary
+You cannot copy a dictionary simply by typing dict2 = dict1,
+because: dict2 will only be a reference to dict1, and changes made
+in dict1 will automatically also be made in dict2.
+There are ways to make a copy, one way is to use the built-in Dictionary
+method copy().
+Example
+Make a copy of a dictionary with the copy() method:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+print(thisdict)
+
+Another way to make a copy is to use the built-in function dict().
+Example
+Make a copy of a dictionary with the dict() function:
+
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+Nested Dictionaries
+A dictionary can contain dictionaries, this is called nested dictionaries.
+Example
+Create a dictionary that contain three dictionaries:
+
+myfamily = {
+"child1" : {
+"name" : "Emil",
+"year" : 2004
+},
+"child2" : {
+"name" : "Tobias",
+"year" : 2007},
+"child3" : {
+"name" : "Linus",
+"year" : 2011
+}
+}
+print(myfamily)
+
+Or, if you want to add three dictionaries into a new dictionary:
+Example
+Create three dictionaries, then create one dictionary that will contain the
+other three dictionaries:
+
+child1 = {
+"name" : "Emil",
+"year" : 2004
+}
+child2 = {
+"name" : "Tobias",
+"year" : 2007
+}
+child3 = {
+"name" : "Linus",
+"year" : 2011
+}
+myfamily = {
+"child1" : child1,
+"child2" : child2,
+"child3" : child3
+}
+print(myfamily)
+
+Overview of Dictionaries
+1. Python Dictionaries
+2. Access Items
+3. Change Items
+4. Add Items
+5. Remove Items
+6. Loop Dictionaries
+7. Copy Dictionaries
+8. Nested Dictionaries
+9. More on Dictionary Methods10. Dictionary Exercises
+Practice Examples:
+Dictionary.py
+#1. Creating a Dictionary:
+# Using curly braces {}
+# creating a dictionary
+country_capitals = {
+"Germany": "Berlin",
+"Canada": "Ottawa",
+"England": "London"
+}
+# printing the dictionary
+print(country_capitals)
+#o/p: {'Germany': 'Berlin', 'Canada': 'Ottawa', 'England': 'London'}
+Example2:
+thisdict = {"name": "Alice", "age": 30, "city": "New York"}
+print(thisdict)
+# Using the dict() constructor
+another_dict = dict(brand="Ford", model="Mustang", year=1964)
+print(another_dict)
+#2. Accessing Dictionary Elements:
+# access the value of keys
+print(country_capitals["Germany"]) # Output: Berlin
+print(country_capitals["England"]) # Output: London
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+# Accessing value by key
+print(my_dict["name"])
+# Using the get() method (safer, returns None if key not found)
+print(my_dict.get("age"))
+print(my_dict.get("country")) # Returns None#3. Modifying Dictionary Elements:
+my_dict = {"name": "Alice", "age": 30}
+# Changing an existing value
+my_dict["age"] = 31
+print(my_dict)
+# Adding a new key-value pair
+my_dict["occupation"] = "Engineer"
+print(my_dict)
+# ex2 - add an item with "Italy" as key and "Rome" as its value
+country_capitals["Italy"] = "Rome"
+print(country_capitals)
+# Using update() to add/modify multiple items
+my_dict.update({"city": "London", "age": 32})
+print(my_dict)
+#4. Removing Dictionary Elements:
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+# Using del to remove a specific key-value pair
+del my_dict["city"]
+print(my_dict)
+# Using pop() to remove and retrieve a value
+removed_age = my_dict.pop("age")
+print(my_dict)
+print(f"Removed age: {removed_age}")
+# Using clear() to remove all items
+my_dict.clear()
+print(my_dict)
+#Ex2 delete item having "Germany" key
+del country_capitals["Germany"]
+print(country_capitals)
+# clear the dictionary(remove all items )
+country_capitals.clear()
+print(country_capitals)
+#5. Iterating Through a Dictionary:
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}# Iterating through keys
+for key in my_dict:
+print(key)
+# Iterating through values
+for value in my_dict.values():
+print(value)
+# Iterating through key-value pairs
+for key, value in my_dict.items():
+print(f"{key}: {value}")
+# change the value of "Italy" key to "Rome"
+country_capitals["Italy"] = "Naples"
+print(country_capitals)
+# Ex2 -- print dictionary keys one by one
+country_capitals = {
+"United States": "Washington D.C.",
+"Italy": "Rome"
+}
+# print dictionary keys one by one
+for country in country_capitals:
+print(country)
+print()
+# print dictionary values one by one
+for country in country_capitals:
+capital = country_capitals[country]
+print(capital)
+# print dictionary length
+numbers = {10: "ten", 20: "twenty", 30: "thirty"}
+# get dictionary's length
+print(len(numbers)) # Output
